d99a603995398c729f8910a51b511212ff03799a
probably333999 <90316671+probably333999@users.noreply.github.com>
2024-02-24 11:33:47 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bot = new Telegraf('6895852282:AAEKSv96V4-oJg7BKnnTTOJMKibTXdq6sZI');
 const targetBotId = '5306177516'; // Replace with the desired target bot's ID
 const jobRequests = {};
 
-bot.start((ctx) => ctx.reply("Assalomu alaykum! Tabriklayman, mening ish yuklaydigan botimga gap qilib berishingizdan xursandman. Boshlanishga, qaysi ish ekanligi haqidaizohlab bering agar sizning ma'lumotlaringiz faylda bo'lsa, faylni junatishingiz mumkin. Ko'proq ma'limot uchun /help tugmasini bosing"));
+bot.start((ctx) => ctx.reply("Salom! Mening ish yuklash botiga xush kelibsiz. Boshlash uchun sharh qoldirib, yuklamoqchi bo'lgan aniq ishni ko'rsating. Agar ma'lumotlaringiz faylda saqlangan bo'lsa, buni yuborishingiz mumkin. Qo'shimcha ma'lumot uchun "/help" tugmasini bosing."));
 
 // Function to generate formatted text for job request
 function formatJobRequest(jobRequest) {
@@ -15,7 +15,7 @@ function formatJobRequest(jobRequest) {
 }
 bot.command('help', (ctx) => {
     ctx.reply("Ish so'rovi botiga xush kelibsiz!\n\n" +
-              "Siz menga bunday ish haqida ma'lumotlarni junating:\n" +
+              "Siz menga bunday ish haqida ma'lumot yuborishingiz mumkin.\n" +
               '- Talablar.\n' +
               '- Kompaniya.\n' +
               '- Ish turi.\n' +
@@ -24,7 +24,7 @@ bot.command('help', (ctx) => {
               '- Murojaat uchun(aloqa uchun raqam)\n' +
               '- Manzil.\n\n' +
               '- Qulayliklar.(Shartmas)\n\n' +
-              "Agar sizning ma'lumotlaringiz faylda bo'lsa, faylni junatishingiz mumkin. Boshlash uchun /start ni bosing");
+              "Agar ma'lumotlaringiz faylda saqlangan bo'lsa, buni yuborishingiz mumkin. Boshlash uchun /start ni bosing");
 });
 bot.on('text', async (ctx) => {
     const userId = ctx.message.from.id;
@@ -34,7 +34,7 @@ bot.on('text', async (ctx) => {
         const messageId = ctx.message.message_id; // Store message ID
         jobRequests[userId] = { userId, messageId, message };
         console.log('New job request:', jobRequests[userId]);
-        return ctx.reply("Iltimos, izlayotgan ish haqida qushimcha ma'lumot bering.");
+        return ctx.reply("Iltimos, siz qidirayotgan ish turi haqida qo'shimcha ma'lumot bering.");
     }
     if (!message) return; // Ignore empty messages
 
@@ -45,7 +45,7 @@ bot.on('text', async (ctx) => {
     if (!userRequest.description) {
         userRequest.description = message;
         console.log('Job description:', userRequest.description);
-        return ctx.reply('Iltimos, ish turi, ish haqi kutilishi va h.k. kabi batafsil maʼlumotlarni kiriting.');
+        return ctx.reply("Iltimos, ish turi, ish haqi va hokazo kabi ma'lumotlarni kiriting.");
     }
 
     // Confirmation message with job request details
@@ -70,7 +70,7 @@ bot.on(['photo', 'document'], async (ctx) => {
         const messageId = ctx.message.message_id; // Store message ID
         jobRequests[userId] = { userId, messageId, fileId, description };
         console.log('New job request:', jobRequests[userId]);
-        return ctx.reply("Iltimos, izlayotgan ish haqida qushimcha ma'lumot bering.");
+        return ctx.reply("Iltimos, siz qidirayotgan ish turi haqida qo'shimcha ma'lumot bering.");
     }
 
     const userRequest = jobRequests[userId];
@@ -78,7 +78,7 @@ bot.on(['photo', 'document'], async (ctx) => {
     if (!userRequest.description) {
         userRequest.description = description;
         console.log('Job description:', userRequest.description);
-        return ctx.reply('Iltimos, ish turi, ish haqi kutilishi va h.k. kabi batafsil maʼlumotlarni kiriting.');
+        return ctx.reply("Iltimos, ish turi, ish haqi va hokazo kabi ma'lumotlarni kiriting.");
     }
 
     // Confirmation message with job request details
@@ -96,7 +96,7 @@ bot.action('skip', async (ctx) => {
     const userId = ctx.update.callback_query.from.id;
 
     // Skip directly to providing a message
-    await ctx.reply("Iltimos, izlayotgan ish haqida qushimcha ma'lumot bering.Agar yo'q bo'lsa yo'q deb yozing");
+    await ctx.reply("Iltimos, siz izlayotgan ishchi haqida ko'proq ma'lumot bering. Agar yo'q bo'lsa, yo'q deb yozing");
 });
 
 
